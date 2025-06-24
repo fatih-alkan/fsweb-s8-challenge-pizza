f31a2e994d02d3301f712765d164248e9c39aafa
fatih-alkan <fatihalkansw@gmail.com>
2025-06-24 18:44:44 +0300

    OrderPage

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.css'
+export default function Header(){
+    return (
+        <header style={{width:"100%"}}><h1 style={{margin:"0", padding:"1rem"}}>Teknolojik Yemekler</h1>
+        <p className='container'>Ana Sayfa - Seçenekler - <span>Sipariş Oluştur</span></p>
+        </header>
+    )
+}
